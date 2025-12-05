c45ea7b2d5a477b1d8e5b5311e7f444f34db9482
nish8d <nishadvjoglekar8@gmail.com>
2025-12-05 16:12:08 +0530

    Update

--- flashcard_pipeline/prompts.py
+++ flashcard_pipeline/prompts.py
@@ -6,11 +6,10 @@ Include:
     title
     question
     answer
-    score
     context
     the_company_mainly_concerned_with_the_news_article
     link
-    source = same as the scored mentioned in the json file
+    source
     summary
     published_at
 
